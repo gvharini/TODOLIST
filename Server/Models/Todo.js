a87fc3f479f8a0611a92e64621352ac983e7c46b
gvharini <harini12ganesan@gmail.com>
2025-07-03 23:31:15 +0530

    todo

--- Server/Models/Todo.js
+++ Server/Models/Todo.js
@@ -5,12 +5,9 @@ const TodoSchema = new mongoose.Schema({
     done: {
         type: Boolean,
         default: false
-<<<<<<< HEAD
     } 
-=======
     }
->>>>>>> 96d1f7622ae7d58a0618373b431421d459c3651f
-})
+)
 
 const TodoModel = mongoose.model('todos',TodoSchema)
 
--- Server/index.js
+++ Server/index.js
@@ -8,11 +8,7 @@ const app = express();
 app.use(cors("*"));
 app.use(express.json());
 
-<<<<<<< HEAD
 mongoose.connect('mongodb+srv://harini:120507@cluster0.ltmw4g9.mongodb.net/todo');
-=======
-mongoose.connect('mongodb://0.0.0.0:27017/test');
->>>>>>> 96d1f7622ae7d58a0618373b431421d459c3651f
 
 app.get('/get', (req, res) => {
     TodoModel.find()
@@ -20,29 +16,17 @@ app.get('/get', (req, res) => {
     .catch(err => res.json(err))
 })
 
-<<<<<<< HEAD
-
 app.put('/update/:id', (req, res) =>{
     const {id} = req.params;
     TodoModel.findByIdAndUpdate({_id: id}, {done: true})
     .then(result => res.json(result))
-=======
-app.put('/update/:id', (req, res) =>{
-    const {id} = req.params;
-    TodoModel.findByIdAndUpdate({_id: id}, {done: true})
-    .then(restul => res.json(result))
->>>>>>> 96d1f7622ae7d58a0618373b431421d459c3651f
     .catch(err => res.json(err))
 })
 
 app.put('/delete/:id', (req, res) => {
     const {id} = req.params;
     TodoModel.findByIdAndDelete({_id: id})
-<<<<<<< HEAD
     .then(result => res.json(result))
-=======
-    .then(restul => res.json(result))
->>>>>>> 96d1f7622ae7d58a0618373b431421d459c3651f
     .catch(err => res.json(err))
 })
 
